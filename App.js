28757ef2de2103fb3b003eda9caa07f4fd0b21e3
Keith Lashley <lashleykeith@gmail.com>
2019-01-31 16:13:24 +0900

    Login not working.  Getting warning
    This is not taking me to the Share place page.  It is giving me a warning that says
    "Possible Unhandled Promise Rejection(id:0): TypeError: undefined is not a function(evaluating'_reactNativeNavigation.Navigation.startTabBasedpp") http://10.0.2.2:8081/index.android.delt?platform-android&

--- App.js
+++ App.js
@@ -1,64 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import { Navigation } from 'react-native-navigation';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// Register Screens
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
 
-type Props = {};
-export default class App extends Component<Props> {
-  state = {
-    placeName: ''
+
+// Start a App
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
   }
-
-  placeNameChangedHander = val => {
-    this.setState({
-        placeName: val
-    })
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-          <TextInput 
-            style={{width: 300, borderColor: "black", borderWidth: 1}}
-            value={this.state.placeName} 
-            onChangeText={this.placeNameChangedHander}
-          />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
 
 // control M
+// npm install --save redux react-redux
+// react-native run-android
+// react native debugger
+// https://github.com/jhen0409/react-native-debugger
+// reduxDevTools Integration
+// https://github.com/jhen0409/react-native-debugger/blob/master/docs/redux-devtools-integration.md
+// https://github.com/wix/react-native-navigation/blob/v1/docs/installation-ios.md
+// https://github.com/oblador/react-native-vector-icons
